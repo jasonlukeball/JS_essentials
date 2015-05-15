de22c0fcc91f6ff64541695e3817c7a11608f3f6
Jason Ball <jasonlukeball@me.com>
2015-05-15 20:53:17 +1000

    Creating DOM Elements

--- Chap05/CreatingDOM/script.js
+++ Chap05/CreatingDOM/script.js
@@ -1,18 +1,37 @@
-//create the elements
-//var newHeading = document.createElement("h1");
-//var newParagraph = document.createElement("p");
+// -----------------------------------------------------
+// CREATING DOM ELEMENTS
+// -----------------------------------------------------
 
-// to add content, either use innerHTML
-//newHeading.innerHTML = "Did You Know?";
-//newParagraph.innerHTML = "California produces over 17 million gallons of wine each year!";
+// STEPS
+// 1. CREATE THE ELEMENT
+// 2. ADD CONTENT TO NEW ELEMENT
+// 3. ADD IT TO THE DOCUMENT
 
-// OR create child nodes manually
-//var h1Text = document.createTextNode("Did You Know?");
-//var paraText = document.createTextNode("California produces over 17 million gallons of wine each year!");
-// and add them as child nodes to the new elements
-//newHeading.appendChild(h1Text);
-//newParagraph.appendChild(paraText);
 
-// and we still need to attach them to the document!
-//document.getElementById("trivia").appendChild(newHeading);
-//document.getElementById("trivia").appendChild(newParagraph);
+// 1. CREATE TWO NEW ELEMENTS
+var newHeading = document.createElement("h1");
+var newParagraph = document.createElement("p");
+
+// 2. ADD CONTENT TO THESE NEW ELEMENTS USING innerHTML
+newHeading.innerHTML = "Did You Know?";
+newParagraph.innerHTML = "California produces over 17 million gallons of wine each year!";
+
+// 3. ADD NEW ELEMENTS TO THE DOCUMENT
+document.getElementById("trivia").appendChild(newHeading);
+document.getElementById("trivia").appendChild(newParagraph);
+
+
+
+// ADD A NEW <li> TO THE #homeNav UNORDERED LIST
+
+// 1. GET THE ELEMENT THAT WE WANT TO INSERT A CHILD INTO
+var mainTitle = document.getElementById("homeNav");
+
+// 2. CREATE THE NEW <li>
+var newLi = document.createElement("li");
+
+// 3. GIVE THE NEW <li> a VALUE
+newLi.innerHTML = "Testing 1,2,3";
+
+// 4. ADD NEW <li> TO THE DOCUMENT
+homeNav.appendChild(newLi);
