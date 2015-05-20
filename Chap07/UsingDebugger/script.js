f8d90c8c6ed57f3b91a3334b99a987c1be8e772a
Jason Ball <jasonlukeball@me.com>
2015-05-20 14:39:46 +1000

    debugging

--- Chap07/UsingDebugger/script.js
+++ Chap07/UsingDebugger/script.js
@@ -14,14 +14,26 @@ function deeperFunction() {
 function simpleFunction() {
 	// jump into a deeper function
 	deeperFunction();
-	// now grab the message and change it.
+
+
+
+
+
+    // IF WE'VE REACHED THE LAST MESSAGE IN THE ARRAY,
+    // RESTART FROM THE FIRST
+    if (messageIndex == messageArray.length) {
+        messageIndex = 0;
+    }
+    
+    // GET A MESSAGE FROM THE ARRAY
 	var newMessage = messageArray[messageIndex];
+    // GET THE ELEMENT TO UPDATE
 	var messageElement = document.getElementById("mainMessage");
+    // UPDATE THE NEW ELEMENT
 	messageElement.innerHTML = newMessage;
+    // INCREMENT THE INDEX
 	messageIndex++;
-	if (messageIndex > messageArray.length) {
-		messageIndex = 0;
-	}
+
 }
 
 function changeMessage() {
@@ -29,7 +41,7 @@ function changeMessage() {
 }
 
 window.onload = function() {
-	setInterval(changeMessage,4000);
+	setInterval(changeMessage,1000);
 };
 
 
