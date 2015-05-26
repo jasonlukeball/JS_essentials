6201e176ee87635e704a5a3fb04478a59299c7f3
Jason Ball <jasonlukeball@me.com>
2015-05-26 17:13:39 +1000

    jquery ROCKS

--- Chap11/jQueryIntro/script.js
+++ Chap11/jQueryIntro/script.js
@@ -1,39 +1,60 @@
-// basic
-document.getElementById("mainArticle").className = "highlight";
+// ------------------------
+// ADD CLASSES TO ELEMENTS
+// ------------------------
 
-// use jQuery - basic
-//jQuery("#mainArticle").addClass("highlight");
+// OLD WAY
+// GET #mainArticle <div> and give it the class "highlight"
+//document.getElementById("mainArticle").className = "highlight";
 
-// find all elements with a particular class
-//jQuery(".tourDescription").addClass("highlight");
+// JQUERY WAY
+// GET #mainArticle <div> and give it the class "highlight"
+jQuery("#mainArticle").addClass("highlight");
 
-// find all elements with a particular tag
-//jQuery("li").addClass("highlight");
 
-// find the last li
-//jQuery("li:last").addClass("highlight");
+// FIND ALL ELEMENTS WITH THE CLASS "tourDescription" AND GIVE THEM THE CLASS "highlight"
+jQuery(".tourDescription").addClass("highlight");
 
-// find any paragraph that contain the word "packages"
-//jQuery("p:contains('packages')").addClass("highlight");
+// FIND ALL ELEMENTS WITH A <li> TAG AND GIVE THEM THE CLASS "highlight"
+jQuery("li").addClass("highlight");
 
+// FIND THE LAST <li> AND GIVE IT THE CLASS "highlightRed"
+jQuery("li:last").addClass("highlightRed");
+
+
+// FIND A PARAGRAPH CONTAINING THE WORD "packages" AND GIVE IT THE CLASS "highlightRed"
+jQuery("p:contains('packages')").addClass("highlightRed");
+
+
+// ------------------------
 // EFFECTS
+// ------------------------
 
-// hide all paragraphs.
-//$("p").hide(4000);
+// HIDE ALL PARAGRAPHS, DURATION 1 SECOND
+$("p").hide(1000);
 
-//$("p").fadeOut(4000);
+//FADE OUT ALL <h2> TAGS, DURATION 2 SECOND
+// $("h2").fadeOut(2000);
 
+
+// ------------------------
 // EVENTS
+// ------------------------
 
-// simple click
-//$("#pageID").click(function() {
-//   $("#pageID").text("You clicked me!");
-//});
 
-// add $(this) to refer to current element
-//$("h2").click(function() {
-//   $(this).text("You clicked me!");
-//});
+// CLICK EVENT
+// ADD CLICK EVENT TO THE "#pageID" <h1>
+$("#pageID").click(function() {
+    // WHEN CLICKED, CHANGE THE TEXT TO "You clicked me!"
+   $("#pageID").text("You clicked me!");
+});
+
+
+// ADD CLICK EVENT TO ALL <h2> TAGS
+$("h2").click(function() {
+    // WHEN ANY <h2> IS CLICKED, CHANGE "this" <h2> TEXT TO "You clicked me!"
+   $(this).text("You clicked me!");
+});
+
 
 // add effects - this makes each paragraph fade out when clicked.
 //$("p").click(function() {
