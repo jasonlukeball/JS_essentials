ff08364a0aba21b2de932cc18c00ebf183244d1b
Jason Ball <jasonlukeball@me.com>
2015-05-27 14:49:55 +1000

    Creating Objects

--- Chap13/ObjectsPrototypes/script.js
+++ Chap13/ObjectsPrototypes/script.js
@@ -1,11 +1,35 @@
 // Simple prototype example
 
-function Player(n) {
-    this.name = n;
+// FUNCTION TO CREATE A NEW OBJECT
+
+
+function Player(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
+// CREATING NEW FUNCTIONS ONTO THE OBJECT
+Player.prototype.loginfo = function() {
+    // LOGS THE PLAYER NAME
+    console.log ("Hi, my name is " + this.name);
+};
 
-var fred =  new Player("Fred");
+Player.prototype.addAge = function() {
+    // ADDS A YEAR ONTO THEIR AGE
+    this.age++
+};
+
+
+var fred =  new Player("Fred", 33);
+console.log (fred);
+
+fred.loginfo();
+fred.addAge();
+
+
+console.log("------------------");
+console.log (fred);
+
 
 
 
